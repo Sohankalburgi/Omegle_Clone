164ef60bea5df4abf36336f8241eed447161112d
Sohankalburgi <sohankalburgi2004@gmail.com>
2025-07-05 09:21:24 +0530

    changed

--- Backend/dist/UserManager.js
+++ Backend/dist/UserManager.js
@@ -1,63 +1,77 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserManager = void 0;
 const RoomManager_1 = require("./RoomManager");
+const User_1 = require("./models/User");
+const Queue_1 = require("./models/Queue");
 class UserManager {
     constructor() {
-        this.users = [];
-        this.queue = [];
         this.roomManager = new RoomManager_1.RoomManager();
     }
-    addUser(name, socket) {
-        this.users.push({
-            name,
-            socket
+    addUser(name, socket, io) {
+        return __awaiter(this, void 0, void 0, function* () {
+            // Save user to DB
+            const user = yield User_1.UserModel.create({ name, socketId: socket.id });
+            // Add to queue in DB
+            let queue = yield Queue_1.QueueModel.findOne();
+            if (!queue) {
+                queue = yield Queue_1.QueueModel.create({ socketIds: [socket.id] });
+            }
+            else {
+                queue.socketIds.push(socket.id);
+                yield queue.save();
+            }
+            socket.emit("lobby");
+            yield this.clearQueue(io);
+            this.initHandlers(socket, io);
         });
-        this.queue.push(socket.id);
-        socket.emit("lobby");
-        this.clearQueue();
-        console.log('this is the init handlers methods exe');
-        this.initHandlers(socket);
     }
     removeUser(socketId) {
-        const user = this.users.find(({ socket }) => socket.id === socketId);
-        this.users = this.users.filter(({ socket }) => socket.id !== socketId);
-        this.queue = this.queue.filter((socket) => socket !== socketId);
+        return __awaiter(this, void 0, void 0, function* () {
+            // Remove from queue in DB
+            let queue = yield Queue_1.QueueModel.findOne();
+            if (queue) {
+                queue.socketIds = queue.socketIds.filter(id => id !== socketId);
+                yield queue.save();
+            }
+            // Optionally remove user from DB (if you want to keep, comment this out)
+            // await UserModel.deleteOne({ socketId });
+        });
     }
-    clearQueue() {
-        console.log(this.queue);
-        console.log('the users list');
-        console.log(this.users);
-        if (this.queue.length < 2) {
-            return;
-        }
-        const id1 = this.queue.pop();
-        const id2 = this.queue.pop();
-        console.log("id1", id1);
-        console.log("id2", id2);
-        const user1 = this.users.find((x) => x.socket.id === id1);
-        const user2 = this.users.find((x) => x.socket.id === id2);
-        console.log("users");
-        console.log(user1);
-        console.log(user2);
-        if (!user1 || !user2) {
-            return;
-        }
-        console.log('creating rooms');
-        const room = this.roomManager.createRooms(user1, user2);
-        this.clearQueue();
+    clearQueue(io) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let queue = yield Queue_1.QueueModel.findOne();
+            if (!queue || queue.socketIds.length < 2)
+                return;
+            const id1 = queue.socketIds.shift();
+            const id2 = queue.socketIds.shift();
+            yield queue.save();
+            const user1 = yield User_1.UserModel.findOne({ socketId: id1 });
+            const user2 = yield User_1.UserModel.findOne({ socketId: id2 });
+            if (!user1 || !user2)
+                return;
+            yield this.roomManager.createRooms(user1, user2, io);
+            yield this.clearQueue(io);
+        });
     }
-    initHandlers(socket) {
+    initHandlers(socket, io) {
         socket.on("offer", ({ sdp, roomId }) => {
-            console.log("under the offer of the offer in inithandler");
-            this.roomManager.onOffer(roomId, sdp, socket.id);
+            this.roomManager.onOffer(roomId, sdp, socket.id, io);
         });
         socket.on("answer", ({ sdp, roomId }) => {
-            console.log("under the offer of the answer in inithandler");
-            this.roomManager.onAnswer(roomId, sdp, socket.id);
+            this.roomManager.onAnswer(roomId, sdp, socket.id, io);
         });
         socket.on("add-ice-candidate", ({ candidate, roomId, type }) => {
-            this.roomManager.onIceCandidate(roomId, socket.id, candidate, type);
+            this.roomManager.onIceCandidate(roomId, socket.id, candidate, type, io);
         });
     }
 }
--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const node_http_1 = require("node:http");
 const socket_io_1 = require("socket.io");
 const UserManager_1 = require("./UserManager");
+const mongoose_1 = __importDefault(require("mongoose"));
 const app = (0, express_1.default)();
 const server = (0, node_http_1.createServer)(app);
 const io = new socket_io_1.Server(server, {
@@ -15,14 +16,20 @@ const io = new socket_io_1.Server(server, {
     }
 });
 const userManager = new UserManager_1.UserManager();
+// Connect to MongoDB
+mongoose_1.default.connect('mongodb://127.0.0.1:27017/Omg').then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('MongoDB connection error:', err);
+});
 io.on('connection', (socket) => {
     console.log('a user connected');
-    userManager.addUser('randomname', socket);
+    userManager.addUser('randomname', socket, io); // Pass io to addUser
     socket.on('disconnect', () => {
         console.log('user disconnected');
         userManager.removeUser(socket.id);
     });
 });
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
     console.log('server running at http://localhost:3000');
 });
